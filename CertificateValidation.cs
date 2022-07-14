64dddc3edd0712572f3f28fc6ed93b7d6cf29a55
lbragado <luis.bragado.mx@gmail.com>
2022-07-14 18:15:32 -0500

    Se carga proyecto a github

---
+++ CertificateValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SolutionWebAppCerts
+{
+    public class CertificateValidation
+    {
+        public bool ValidateCertificate(X509Certificate2 clientCertificate)
+        {
+            string[] allowedThumbprints = {
+                "493C5F31E6B24E067269E293FED820978A9A1C10"
+            };
+            if (allowedThumbprints.Contains(clientCertificate.Thumbprint))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
